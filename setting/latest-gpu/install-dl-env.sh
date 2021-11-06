74fe92e1321856cb937ef63f1ca354fdf17fae38
kosehy <kosehy@gmail.com>
2021-11-07 00:53:01 +0900

    Add latest gpu setting

---
+++ setting/latest-gpu/install-dl-env.sh
@@ -0,0 +1,62 @@
+# install-dl-env.sh
+#!/bin/sh
+
+# Add additional repositories
+add-apt-repository ppa:graphics-drivers/ppa -y
+
+# install cuda from nvidia website
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.2/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.2-470.57.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.2-470.57.02-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+# Install packages
+apt-get install net-tools ubuntu-drivers-common nvidia-driver-450-server nvidia-cuda-toolkit -y
+
+# Get the latest package lists
+apt-get update
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" -y
+
+# Get the latest package lists
+apt-get update
+
+# Install helm3
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 \
+   && chmod 700 get_helm.sh \
+   && ./get_helm.sh
+
+# install docker-ce, docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+# install nvidia-docker & nvidia-container-toolkit & openssh-client
+apt-get update
+apt-get install nvidia-docker2 -y
+
+# restart the docker
+systemctl restart docker
+
+# reboot the server
+reboot
---
+++ setting/latest-gpu/install-k8s.sh
@@ -0,0 +1,19 @@
+
+# install-kubernetes.sh
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+
+# install kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=1.19.16-00 kubeadm=1.19.16-00 kubectl=1.19.16-00
+# hold the .
+sudo apt-mark hold kubelet kubeadm kubectl
+# check kubernetes version
+kubeadm version
+kubelet --version
+kubectl version
+
