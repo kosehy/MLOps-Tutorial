60ed7b497dbbea619c349d623191ebf13e6213b1
kosehy <kosehy@gmail.com>
2021-11-07 01:16:33 +0900

    change kubernetes 1.19.16 to 1.19.15

--- setting/install-until-k8s-dashboard.sh
+++ setting/install-until-k8s-dashboard.sh
@@ -1,22 +1,3 @@
-
-# install-kubernetes.sh
-
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-sudo apt-get update
-
-# install kubelet kubeadm kubectl 1.19.15
-sudo apt-get install -y kubelet=1.19.15-00 kubeadm=1.19.15-00 kubectl=1.19.15-00
-# hold the .
-sudo apt-mark hold kubelet kubeadm kubectl
-# check kubernetes version
-kubeadm version
-kubelet --version
-kubectl version
-
 # kubeadm init
 sudo kubeadm init --apiserver-advertise-address 192.168.35.10 --pod-network-cidr=192.168.0.0/24
 
@@ -45,9 +26,9 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl diff -f - -n kube-system
 
-# install metallb 0.10.3
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.3/manifests/metallb.yaml
+# install metallb 0.11.0
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
 
 # setup the metallb configmap
 kubectl apply -f values_override_10.yaml
