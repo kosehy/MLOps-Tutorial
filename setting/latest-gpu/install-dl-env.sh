e89335046715fc0c3d0ca6778dfe615931802143
SEUNG HYUN KO <kosehy@gmail.com>
2021-11-16 21:29:32 +0900

    Update install-dl-env.sh
    add cuda toollkit link

--- setting/latest-gpu/install-dl-env.sh
+++ setting/latest-gpu/install-dl-env.sh
@@ -5,6 +5,7 @@
 add-apt-repository ppa:graphics-drivers/ppa -y
 
 # install cuda from nvidia website
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_local
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.4.2/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.2-470.57.02-1_amd64.deb
